876a3d964380418523766cd92bd6808397d7ee89
Felipe Angelo <felipe.angelo@ymail.com>
2024-10-22 19:59:21 -0300

    Criando o servidor Express

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.use('/api/user', userController);
+
+// TODO - Configurar Swagger
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
